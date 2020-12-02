ffdb1fea1cd7478aa99f47c9f6ba7653b62ba773
Vladimir Kharchev <necrosovereign@gmail.com>
2020-12-02 17:34:39 +0100

    Finished functionality of the program state

--- library/programstate.py
+++ library/programstate.py
@@ -1,7 +1,7 @@
 # /usr/bin/env python
 import pandas as pd
 import os
-from typing import Union, TextIO
+from typing import Union, TextIO, List, Any
 
 
 class FileFormat():
@@ -34,11 +34,40 @@ class TableFormat(FileFormat):
                      line_terminator=line_terminator)
 
 
+def the_unique(column: pd.Series) -> Any:
+    result = column.dropna().unique()
+    if len(result) == 0:
+        return None
+    elif len(result) == 1:
+        return result.squeeze()
+    else:
+        raise ValueError
+
+
+def merge_rows(rows: pd.DataFrame, column_name: str) -> pd.DataFrame:
+    try:
+        result = rows.agg(the_unique)
+    except ValueError:
+        result = rows.copy()
+        result['remarks'] = f"Rows for this {column_name} not merged due to multiple occurrences with non-identical content in one table"
+    else:
+        result.to_frame().T
+    return result
+
+
 class ProgramState():
     """
     The class encapsulating the state of specimentablemerger
     """
 
     def __init__(self) -> None:
-        self.input_format = TableFormat("\t")
-        self.output_format = TableFormat("\t")
+        self.input_format: FileFormat = TableFormat("\t")
+        self.output_format: FileFormat = TableFormat("\t")
+        self.unifying_field = "specimenid"
+
+    def merger(self, input_files: List[str], output_file: str) -> None:
+        table = pd.concat(self.input_format.load_table(filename)
+                          for filename in input_files)
+        output_table = table.groupby(self.unifying_field).apply(
+            merge_rows, self.unifying_field)
+        self.output_format.write_table(output_file, output_table)
