e0dce2963cbbd62e3fcb0d4ca1ca73be0f7982b7
Vladimir Kharchev <necrosovereign@gmail.com>
2021-01-27 17:05:26 +0100

    Implemented fuzzy merging functionality

--- library/programstate.py
+++ library/programstate.py
@@ -1,7 +1,10 @@
 # /usr/bin/env python
 import pandas as pd
 import os
-from typing import Union, TextIO, List, Any
+import re
+from typing import Union, TextIO, List, Any, cast
+
+fuzzy_merge_regex = re.compile(r'[- _./\\]+')
 
 
 class FileFormat():
@@ -34,20 +37,37 @@ class TableFormat(FileFormat):
         table.to_csv(path_or_buf, sep=self.sep, index=False,
                      line_terminator=line_terminator)
 
-
 def the_unique(column: pd.Series) -> Any:
-    result = column.dropna().unique()
-    if len(result) == 0:
+    values = column.dropna()
+    try:
+        result: str = cast(str, values.iat[0])
+    except IndexError:
         return None
-    elif len(result) == 1:
-        return result.squeeze()
+    if values.eq(result).all():
+        return result
+    else:
+        raise ValueError
+
+def the_unique_fuzzy(column: pd.Series) -> Any:
+    values = column.dropna()
+    try:
+        result: str = cast(str, values.iat[0])
+    except IndexError:
+        return None
+    values = values.str.casefold().replace(fuzzy_merge_regex, "")
+    first_value = fuzzy_merge_regex.sub("", result.casefold())
+    if values.eq(first_value).all():
+        return result
     else:
         raise ValueError
 
 
-def merge_rows(rows: pd.DataFrame, column_name: str) -> pd.DataFrame:
+def merge_rows(rows: pd.DataFrame, column_name: str, fuzzy_merge: bool) -> pd.DataFrame:
     try:
-        result = rows.agg(the_unique)
+        if fuzzy_merge:
+            result = rows.agg(the_unique_fuzzy)
+        else:
+            result = rows.agg(the_unique)
     except ValueError:
         result = rows.copy()
         result['remarks'] = f"Rows for this {column_name} not merged due to multiple occurrences with non-identical content in one table"
@@ -68,6 +88,7 @@ class ProgramState():
         self.input_format: FileFormat = TableFormat("\t")
         self.output_format: FileFormat = TableFormat("\t")
         self.unifying_field = "specimenid"
+        self.fuzzy_merge = False
 
     def set_input_format(self, format: FileFormat) -> None:
         self.input_format = format
@@ -78,9 +99,14 @@ class ProgramState():
     def set_unifying_field(self, field: str) -> None:
         self.unifying_field = field
 
+    def groupby_value(self, table: pd.DataFrame) -> Union[str, pd.Series]:
+        if not self.fuzzy_merge:
+            return self.unifying_field
+        return table[self.unifying_field].str.casefold().replace(fuzzy_merge_regex, "")
+
     def merger(self, input_files: List[str], output_file: str) -> None:
         table = pd.concat([self.input_format.load_table(filename)
                            for filename in input_files])
-        output_table = table.groupby(self.unifying_field).apply(
-            merge_rows, self.unifying_field)
+        output_table = table.groupby(self.groupby_value(table)).apply(
+            merge_rows, self.unifying_field, self.fuzzy_merge)
         self.output_format.write_table(output_file, output_table)
